536cc0a0cdb27b678a566fe6448e7b0a9ddbccc4
m.saher <mohsaher9900@gmail.com>
2026-02-21 17:20:24 +0200

    commit1

---
+++ api/products/[id].js
@@ -0,0 +1,52 @@
+const EXTERNAL_BASE = "http://ecommercedb.runasp.net/api/products";
+
+function corsHeaders() {
+  return {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+}
+
+export async function OPTIONS() {
+  return new Response(null, { status: 204, headers: corsHeaders() });
+}
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const pathParts = url.pathname.split("/").filter(Boolean);
+  const id = pathParts[pathParts.length - 1];
+
+  if (!id) {
+    return new Response(
+      JSON.stringify({ error: "Product ID required" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json", ...corsHeaders() },
+      }
+    );
+  }
+
+  try {
+    const res = await fetch(`${EXTERNAL_BASE}/${id}`);
+    const data = await res.json();
+    return new Response(JSON.stringify(data), {
+      status: res.status,
+      headers: {
+        "Content-Type": "application/json",
+        ...corsHeaders(),
+      },
+    });
+  } catch (err) {
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch product" }),
+      {
+        status: 502,
+        headers: {
+          "Content-Type": "application/json",
+          ...corsHeaders(),
+        },
+      }
+    );
+  }
+}
