02a9e99c002baf98f942b6e847ebc294a53c418f
m.saher <mohsaher9900@gmail.com>
2026-02-23 15:09:40 +0200

    proxyAch

--- src/contexts/AchContext.jsx
+++ src/contexts/AchContext.jsx
@@ -1,28 +1,35 @@
 import { createContext } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { proxyImageUrl } from "../utils/imageUrl.js";
 
- const AchContext = createContext();
- export default AchContext;
+const AchContext = createContext();
+export default AchContext;
 
 export const AchContextProvider = ({ children }) => {
   const [ach, setAch] = useState({});
   const [achLoading, setAchLoading] = useState(false);
-  const achURL = "http://ecommercedb.runasp.net/api/Achievement";
+  const achURL = "/api/achievement";
+
   const getAch = async () => {
     setAchLoading(true);
     try {
       const { data } = await axios.get(achURL);
-      setAch(data);
-    } catch(err) {
+      const normalized =
+        data && typeof data === "object"
+          ? { ...data, image: proxyImageUrl(data?.image) }
+          : data;
+      setAch(normalized);
+    } catch (err) {
       console.log(err);
     } finally {
       setAchLoading(false);
     }
-  }
+  };
+
   return (
     <AchContext.Provider value={{ getAch, ach, achLoading }}>
       {children}
     </AchContext.Provider>
-  )
-}
+  );
+};
