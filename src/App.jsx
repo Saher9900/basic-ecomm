860e90246a3026ad265ac47056f0851fc4a4a96a
m.saher <mohsaher9900@gmail.com>
2026-02-25 00:19:55 +0200

    scrolltotop

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 
 import Layout from './Layout.jsx'
 import Home from './pages/Home/Home.jsx'
@@ -10,6 +11,16 @@ import ProDetails from './pages/ProDetails/ProDetails.jsx'
 import Contact from './pages/Contact/Contact.jsx'
 import TestImage from './pages/TestImage.jsx'
 
+function ScrollToTop() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return null;
+}
+
 function App() {
   const router = createBrowserRouter(
     [
@@ -18,14 +29,14 @@ function App() {
         element: <Layout />,
         errorElement: <ErrorPage />,
         children: [
-          { index: true, element: <Home /> },
-          { path: "about", element: <About /> },
-          { path: "products", element: <Products /> },
-          { path: "contact", element: <Contact /> },
-          { path: "products/proDetails/:id", element: <ProDetails /> },
-          { path: "products/:category", element: <Products /> },
-          { path: "*", element: <NotFound /> },
-          { path: "test", element: <TestImage /> },
+          { index: true, element: <><ScrollToTop /><Home /></> },
+          { path: "about", element: <><ScrollToTop /><About /></> },
+          { path: "products", element: <><ScrollToTop /><Products /></> },
+          { path: "contact", element: <><ScrollToTop /><Contact /></> },
+          { path: "products/proDetails/:id", element: <><ScrollToTop /><ProDetails /></> },
+          { path: "products/:category", element: <><ScrollToTop /><Products /></> },
+          { path: "*", element: <><ScrollToTop /><NotFound /></> },
+          { path: "test", element: <><ScrollToTop /><TestImage /></> },
         ],
       },
     ],
